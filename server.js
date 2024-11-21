9efb0495616583b1ff434e4cc27425d60995d9a8
Shubhra <shubhrajain380@gmail.com>
2024-11-21 17:03:20 +0530

    Added CORS and file validation logic

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const base64topdf = require("base64topdf");
 const fs = require("fs");
+const cors=require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors()); 
 
 // Middleware
 app.use(bodyParser.json());
